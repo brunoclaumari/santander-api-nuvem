08eee29335e698f95063a28a9a4d22cf92095a6e
brunoclaumari <brunoclaumari@gmail.com>
2023-11-14 00:39:48 -0300

    Primeiro commit - criando API

---
+++ src/main/java/me/dio/santanderapinuvem/SantanderApiNuvemApplication.java
@@ -0,0 +1,13 @@
+package me.dio.santanderapinuvem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderApiNuvemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderApiNuvemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/santanderapinuvem/SantanderApiNuvemApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.santanderapinuvem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderApiNuvemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
